52af8111eb7eb98c30b266238eb88138024afd6e
VOVA-PC\Vova <vova.moskalyuk11jr@gmail.com>
2021-02-24 12:20:14 +0200

    add phonebook

--- src/components/App.js
+++ src/components/App.js
@@ -6,37 +6,49 @@ import Filter from './Filter';
 
 export default class App extends Component {
     state = {
-        contacts: [],
+        contacts: [
+            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ],
         filter: '',
         name: '',
         number: '',
     };
 
     addContact = (name, number) => {
-        const contact = {
-            id: uuid(),
-            name,
-            number,
-        };
+        const searchSameName = this.state.contacts
+            .map(cont => cont.name)
+            .includes(name);
 
-        this.setState(prevState => {
-            return {
-                contacts: [...prevState.contacts, contact],
+        console.log(searchSameName);
+
+        if (searchSameName) {
+            alert(`${name} is already in contacts`);
+        } else if (name.length === 0) {
+            alert('Fields must be filled!');
+        } else {
+            const contact = {
+                id: uuid(),
+                name,
+                number,
             };
-        });
-
+            this.setState(prevState => ({
+                contacts: [...prevState.contacts, contact],
+            }));
+        }
     };
 
-    // // return contacts.filter(contact =>
-    // //     contact.name.toLowerCase().includes(filter.toLowerCase()),
-    // // );
-
-    // checkDublicates = (name, number) => {
-    //     const { contacts } = this.state;
-    //     return contacts.filter(contact =>
-    //         contact.name.toLowerCase().includes(name.toLowerCase()),
-    //     );
-    // };
+    removeContact = contactId => {
+        this.setState(prevState => {
+            return {
+                contacts: prevState.contacts.filter(
+                    ({ id }) => id !== contactId,
+                ),
+            };
+        });
+    };
 
     changeFilter = filter => {
         this.setState({ filter });
@@ -63,7 +75,10 @@ export default class App extends Component {
                 <Filter value={filter} onChangeFilter={this.changeFilter} />
 
                 <h1>Contacts</h1>
-                <PhoneList contacts={visibleContacts} />
+                <PhoneList
+                    contacts={visibleContacts}
+                    onRemoveContact={this.removeContact}
+                />
             </div>
         );
     }
--- src/components/PhoneList/PhoneList.js
+++ src/components/PhoneList/PhoneList.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './css/PhoneList.css'
+import './css/PhoneList.css';
 
-const PhoneList = ({ contacts }) => (
+const PhoneList = ({ contacts, onRemoveContact }) => (
     <ul>
         {contacts.map(({ id, name, number }) => (
             <li key={id}>
                 <p>{name}</p>
                 <p>{number}</p>
+                <button type="button" onClick={() => onRemoveContact(id)}>
+                    Remove
+                </button>
             </li>
         ))}
     </ul>
